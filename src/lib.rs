4968a710d4180676a9f5082467590f49a827a011
Bubblepoint <t.matt81+github@gmail.com>
2016-02-20 14:13:45 +0100

    basic answers added

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
-#[test]
-fn it_works() {
+#[macro_use]
+extern crate nickel;
+
+use nickel::{ Nickel, HttpRouter };
+
+pub fn listen() {
+    let mut server = Nickel::new();
+
+    server.get("/flash/?", middleware! {
+        "JDownloader"
+    });
+
+    server.get("/jdcheck.js/?", middleware! {
+        "jdownloader=true; var version='9.581;'"
+    });
+
+    server.listen("127.0.0.1:9666");
 }
